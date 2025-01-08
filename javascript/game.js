68754a2677808a65f4f530d98fe6d32977798c23
Alessandro Argenziano <1603alessandro@gmail.com>
2025-01-08 23:17:04 +0100

    added impressum and contact on the starting screen

--- javascript/game.js
+++ javascript/game.js
@@ -3,6 +3,14 @@ let world;
 let keyboard = new Keyboard();
 
 function init() {
+  let howtoContainer = document.querySelector(".howto_container");
+  let copyrightContainer = document.querySelector(".copyright_container");
+  let instructions = document.querySelector(".instructions");
+
+  howtoContainer.style.display = "none";
+  copyrightContainer.style.display = "none";
+  instructions.style.opacity = "0";
+
   canvas = document.getElementById("canvas");
   world = new World(canvas, keyboard);
 }
@@ -13,6 +21,57 @@ function showInstructions() {
     instructions.style.opacity = 1;
   } else if ((instructions.style.opacity = 1)) {
     instructions.style.opacity = 0;
+  }
+}
+
+function showHowTo() {
+  let howtoContainer = document.querySelector(".howto_container");
+
+  if (
+    howtoContainer.style.display === "none" ||
+    howtoContainer.style.display === ""
+  ) {
+    howtoContainer.style.display = "flex";
+    howtoContainer.addEventListener("click", closeIfClickOutside);
+  } else {
+    howtoContainer.style.display = "none";
+    howtoContainer.removeEventListener("click", closeIfClickOutside);
+  }
+}
+
+function showCopyright() {
+  let copyrightContainer = document.querySelector(".copyright_container");
+
+  if (
+    copyrightContainer.style.display === "none" ||
+    copyrightContainer.style.display === ""
+  ) {
+    copyrightContainer.style.display = "flex";
+    copyrightContainer.addEventListener("click", closeIfClickOutside);
+  } else {
+    copyrightContainer.style.display = "none";
+    copyrightContainer.removeEventListener("click", closeIfClickOutside);
+  }
+}
+
+function closeIfClickOutside(event) {
+  let howtoContainer = document.querySelector(".howto_container");
+  let howtoContent = document.querySelector(".howto_content");
+  let copyrightContainer = document.querySelector(".copyright_container");
+  let copyrightContent = document.querySelector(".copyright_content");
+
+  if (
+    howtoContainer.style.display === "flex" &&
+    !howtoContent.contains(event.target)
+  ) {
+    howtoContainer.style.display = "none";
+    document.removeEventListener("click", closeIfClickOutside);
+  } else if (
+    copyrightContainer.style.display === "flex" &&
+    !copyrightContent.contains(event.target)
+  ) {
+    copyrightContainer.style.display = "none";
+    document.removeEventListener("click", closeIfClickOutside);
   }
 }
 
